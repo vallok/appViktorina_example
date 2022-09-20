a54c8dc1dca86faa41dc24b5902b663d331183ca
Andrey <45331404+vallok@users.noreply.github.com>
2022-09-20 12:24:49 +0200

    start activity

--- app/src/main/java/com/example/viktorina_app/MainActivity.java
+++ app/src/main/java/com/example/viktorina_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.viktorina_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
