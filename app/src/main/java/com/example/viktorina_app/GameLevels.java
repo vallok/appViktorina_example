3972e1e9856c79fa84035cbb23dedfcca3351c6c
Andrey <45331404+vallok@users.noreply.github.com>
2022-09-22 09:19:57 +0200

    GameLevels onBackPressed

--- app/src/main/java/com/example/viktorina_app/GameLevels.java
+++ app/src/main/java/com/example/viktorina_app/GameLevels.java
@@ -1,10 +1,13 @@
 package com.example.viktorina_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class GameLevels extends AppCompatActivity {
 
@@ -14,5 +17,29 @@ public class GameLevels extends AppCompatActivity {
         setContentView(R.layout.game_levels);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        AppCompatButton button_back = findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch(Exception e){
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }catch(Exception e){
+
+        }
     }
 }
