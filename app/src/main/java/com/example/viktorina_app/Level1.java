aff522f423b815bf161a8d1d5e259933260c2efb
Andrey <45331404+vallok@users.noreply.github.com>
2022-10-01 09:26:39 +0200

    Level1. Click on Button Back

--- app/src/main/java/com/example/viktorina_app/Level1.java
+++ app/src/main/java/com/example/viktorina_app/Level1.java
@@ -2,12 +2,21 @@ package com.example.viktorina_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
+
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,68 @@ public class Level1 extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // show the dialog
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // hide the title
+        dialog.setContentView(R.layout.preview_dialog); // set view of the dialog
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // set transparent background
+        dialog.setCancelable(false);
+        //начало обработки нажатия кнопок диалога
+        TextView button_close = dialog.findViewById(R.id.button_close);
+        button_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                обработка нажатия кнопки закрытия диалога Начало
+                try{
+                    Intent intent = new Intent(Level1.this, GameLevels.class);// создаем наеменение
+                    startActivity(intent);// запускаем намерение
+                    finish(); //закрываем окноп с уровнем
+
+                }catch (Exception e){
+
+                }
+                dialog.dismiss(); // закрываем диалог
+//                обработка нажатия кнопки закрития диалога конец
+            }
+        });
+
+        Button button_continue = dialog.findViewById(R.id.button_continue);
+        button_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss(); // close dialog
+            }
+        });
+        //конец обрботки нажатия кнопок диалога
+        dialog.show();
+
+        // Click on Back button - start
+        Button button_back = findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(Level1.this, GameLevels.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        // Click on Back button - end
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(Level1.this, GameLevels.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+
+        }
     }
 }
