ad772f3f40ced9d992b7ac34a8ca4abf44e44cde
Andrey <45331404+vallok@users.noreply.github.com>
2022-10-02 10:31:28 +0200

    Level1. Images + Array class for nubers

---
+++ app/src/main/java/com/example/viktorina_app/Array.java
@@ -0,0 +1,34 @@
+package com.example.viktorina_app;
+
+public class Array {
+
+    final int[] images1 = {
+            R.drawable.one_level_zero,
+            R.drawable.one_level_one,
+            R.drawable.one_level_two,
+            R.drawable.one_level_three,
+            R.drawable.one_level_four,
+            R.drawable.one_level_five,
+            R.drawable.one_level_six,
+            R.drawable.one_level_seven,
+            R.drawable.one_level_eight,
+            R.drawable.one_level_nine,
+    };
+
+    final int[] power1 = {0,1,2,3,4,5,6,7,8,9,};
+
+    final int[] texts1 = {
+        R.string.lvl1_zero,
+        R.string.lvl1_one,
+        R.string.lvl1_two,
+        R.string.lvl1_three,
+        R.string.lvl1_four,
+        R.string.lvl1_five,
+        R.string.lvl1_six,
+        R.string.lvl1_seven,
+        R.string.lvl1_eight,
+        R.string.lvl1_nine,
+
+    };
+
+}
